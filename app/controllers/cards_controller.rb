a9cf167a9c84d997258fae17611ce9d209b8ed70
fooeyandnuts <william.sipes@gmail.com>
2014-08-19 14:49:18 -0700

    added quizzes and cards creation

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,21 @@
 class CardsController < ApplicationController
+  before_action :set_quiz, only: [:new, :create, :edit, :update, :show, :destroy]
+  before_action :set_card, only: [:edit, :update, :show, :destroy]
+
   def index
   end
 
   def new
+    @card = Card.new
   end
 
   def create
+    @card = @quiz.cards.new(card_params)
+    if @card.save
+      redirect_to quiz_path(params[:quiz_id])
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +28,19 @@ class CardsController < ApplicationController
   end
 
   def destroy
+  end
+
+  protected
+
+  def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:question, :answer, :quiz_id)
   end
 end
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,22 +1,58 @@
 class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:edit, :update, :show, :destroy]
+  respond_to :json, :html
+
   def index
+    @quizzes = Quiz.all
+    respond_with @quizzes
   end
 
   def new
-  end
-
-  def show
+    @quiz = Quiz.new
   end
 
   def create
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      respond_to do |format|
+        format.html { redirect_to quiz_path(@quiz) }
+        format.json { render json: @quiz, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.html { render action: 'new' }
+        format.json { render json: @quiz.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
+    if @quiz.update(quiz_params)
+      redirect_to quizzes_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @quiz.destroy
+    redirect_to quizzes_path
   end
+
+  protected
+
+  def quiz_params
+    params.require(:quiz).permit(:subject, :name, :user_id)
+  end
+
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
+  end
+
 end
