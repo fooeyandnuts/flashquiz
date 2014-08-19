8c938a6aad30725511c0b51d7b3cc21a56a09a3f
fooeyandnuts <william.sipes@gmail.com>
2014-08-18 17:01:50 -0700

    added user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
   
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def edit
+    @user = User.new(user_params)
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  protected
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
